761ef98c9d76c89450c469f7a016f3e6bccf91fc
Sirius1616 <siriusa1.615@gmail.com>
2023-09-21 21:26:36 +0000

    new

---
+++ opfuncs2.c
@@ -0,0 +1,96 @@
+#include "monty.h"
+
+/**
+ * pint - prints top element of stack
+ * @stack: pointer to address of head of a stack
+ * @line_number: current line of execution
+*/
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	checkstack(stack, line_number);
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pop - deletes top element of stack
+ * @stack: pointer to address of head of a stack
+ * @line_number: current line of execution
+*/
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free(load.buf);
+		free_stack(*stack);
+		fclose(load.file);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+	*stack = (*stack)->next;
+	free(tmp);
+	tmp = NULL;
+
+}
+
+/**
+ * nop - does nothing
+ * @stack: pointer to address of head of a stack
+ * @line_number: current line of execution
+*/
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) line_number;
+	(void) stack;
+}
+
+/**
+ * swap - swaps top 2 members of stack
+ * @stack: pointer to address of head of a stack
+ * @line_number: current line of execution
+*/
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+	int tmp;
+
+	if (node == NULL || node->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free(load.buf);
+		free_stack(*stack);
+		fclose(load.file);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = node->n;
+	node->n = node->next->n;
+	node->next->n = tmp;
+}
+
+/**
+ * add - adds top two elements of the stack
+ * @stack: pointer to address of head of a stack
+ * @line_number: current line of execution
+*/
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+
+	if (node == NULL || node->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free_stack(*stack);
+		fclose(load.file);
+		exit(EXIT_FAILURE);
+	}
+	node->next->n += node->n;
+	*stack = (*stack)->next;
+	free(node);
+}
